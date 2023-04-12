a814dc43ccd0db1613fa7e212836ddef8ccb95f4
Shashank Naik <shashanknaik808@gmail.com>
2023-04-12 23:52:14 +0530

    Added eventListener codes in app.js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 console.log("Welcome to notes app. This is app.js");
+showNotes();
 
 // If user adds a note, add it to the localStorage
 let addBtn = document.getElementById("addBtn");
@@ -15,7 +16,7 @@ addBtn.addEventListener("click", function (e) {
     notesObj.push(addTxt.value);
     localStorage.setItem("notes", JSON.stringify(notesObj))
     addTxt.value = "";
-    console.log(notesObj);
+    // console.log(notesObj);
     showNotes();
 });
 
@@ -50,16 +51,34 @@ function showNotes() {
 
 // Function to delete a note
 function deleteNote(index) {
-    console.log("I am deleting", index);
+    // console.log("I am deleting", index);
 
     let notes = localStorage.getItem("notes");
-  if (notes == null) {
-    notesObj = [];
-  } else {
-    notesObj = JSON.parse(notes);
-  }
+    if (notes == null) {
+        notesObj = [];
+    } else {
+        notesObj = JSON.parse(notes);
+    }
 
-  notesObj.splice(index, 1);
-  localStorage.setItem("notes", JSON.stringify(notesObj));
-  showNotes();
+    notesObj.splice(index, 1);
+    localStorage.setItem("notes", JSON.stringify(notesObj));
+    showNotes();
 }
+
+let search = document.getElementById('searchTxt');
+search.addEventListener("input", function () {
+
+    let inputVal = search.value.toLowerCase();
+    // console.log('Input event fired!', inputVal);
+    let noteCards = document.getElementsByClassName('noteCard');
+    Array.from(noteCards).forEach(function (element) {
+        let cardTxt = element.getElementsByTagName("p")[0].innerText;
+        if (cardTxt.includes(inputVal)) {
+            element.getElementsByClassName.display = "block";
+        }
+        else {
+            element.style.display = "none";
+        }
+        // console.log(cardTxt);
+    })
+})
