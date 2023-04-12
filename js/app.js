47360230d05ac1da80a8df740337815a72c24ae3
Shashank Naik <shashanknaik808@gmail.com>
2023-04-12 23:28:55 +0530

    Function to show elements from localStorage

--- js/app.js
+++ js/app.js
@@ -16,4 +16,34 @@ addBtn.addEventListener("click", function (e) {
     localStorage.setItem("notes", JSON.stringify(notesObj))
     addTxt.value = "";
     console.log(notesObj);
-})
+    showNotes();
+});
+
+// Function to show elements from localStorage
+function showNotes() {
+    let notes = localStorage.getItem("notes");
+    if (notes == null) {
+        notesObj = [];
+    }
+    else {
+        notesObj = JSON.parse(notes);
+    }
+    let html = "";
+    notesObj.forEach(function (element, index) {
+        html += `
+        <div class="noteCard my-2 mx-2 card" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">Note ${index + 1}</h5>
+                    <p class="card-text"> ${element}</p>
+                    <button id="${index}"onclick="deleteNote(this.id)" class="btn btn-primary">Delete Note</button>
+                </div>
+            </div>`;
+    });
+    let notesElm = document.getElementById("notes");
+    if (notesObj.length != 0) {
+        notesElm.innerHTML = html;
+    }
+    else {
+        notesElm.innerHTML = `Nothing to show! Use "Add a Note" section above to add notes.`;
+    }
+}
